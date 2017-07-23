5b89d811a1e7493e7e00b6fe85747b6c12b27704
tranki1 <kimhieutran100@gmail.com>
2017-07-23 21:47:29 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function() {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Kim',
-    text: 'Hey thats work'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'Kim',
+  //   text: 'Hey thats work'
+  // });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'Floki',
-    text: 'Hey, it me',
-    creatAt: 123
-  });
+  // socket.emit('newMessage', {
+  //   from: 'Floki',
+  //   text: 'Hey, it me',
+  //   creatAt: 123
+  // });
 
   socket.on('createMessage', message => {
     console.log('create Message', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
